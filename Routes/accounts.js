a6b4bc06b91e553df82dc4a84e29ab72b5f9db9a
CScori <amilanotte1366@gmail.com>
2019-11-04 16:54:02 -0600

    adds set up and sql stretch

--- Routes/accounts.js
+++ Routes/accounts.js
@@ -1,21 +1,17 @@
-const express = require('express').Router();
+const express = require('express')
+const acct = require('express').Router();
 
-const knex = require('../data/dbConfig/js');
+const knex = require('../data/dbConfig.js');
 
-const acct = express();
 
 acct.use(express.json());
 
 acct.get('/', (req, res) => {
     knex
     .select('*')
-    .from('account')
-    .then(posts => {
-      res.status(200).json(account);
-    })
-    .catch(error => {
-      res.status(500).json({ error: 'Failed to get posts from database' });
-    });
+    .from('accounts')
+    .then()
+    .catch()
 })
 
 module.exports = acct;
